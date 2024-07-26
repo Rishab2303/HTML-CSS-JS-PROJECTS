198f281d76a27442e21591257573e25627691f73
Rishab Bhardwaj <rishabbhardwaj2203@gmail.com>
2024-07-26 22:30:10 +0530

    calculator

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,6 +2,7 @@ let ke = document.querySelectorAll(".ke")
 let screen = document.querySelector(".screen")
 let answer = document.querySelector(".answer")
 let exparr = []
+
 for (let i of ke) {
     i.addEventListener("click", (e) => {
         let expression = e.target.innerText
@@ -31,8 +32,13 @@ Delete.addEventListener("click", () => {
 
 let equal = document.querySelector(".equal")
 equal.addEventListener("click", () => {
-    let result = eval(exparr.join(""));
-    answer.innerText = result;
+    if (exparr.length !== 0) {
+        let result = eval(exparr.join(""));
+        answer.innerText = result;
+    }
+    else {
+        alert("Enter Something to calculate")
+    }
 })
 let multiply = document.querySelector(".multiply")
 multiply.addEventListener("click", () => {
@@ -43,8 +49,27 @@ multiply.addEventListener("click", () => {
 
 })
 
+let hasdecimal = false
+
+let point = document.querySelector(".point")
+point.addEventListener("click", (e) => {
+    let expression = e.target.innerText
+    if (!hasdecimal) {
+        exparr.push(expression)
+        screen.innerText = exparr.join("")
+        hasdecimal = true
+
+    }
+
+})
+
+let operation = document.querySelectorAll(".operation")
+console.log(operation)
+
+for (let i of operation) {
+    i.addEventListener("click", () => {
+        hasdecimal = false
+    })
 
 
-
-
-
+}
